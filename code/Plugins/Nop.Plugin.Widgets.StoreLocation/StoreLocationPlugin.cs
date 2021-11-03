025a30f4b7e652a11bbdf26d07f411c0608aaa44
Trung Nguyen Duc <trung.nguyen3@ntq-solution.com.vn>
2021-11-03 08:22:42 +0700

    create StoreLocation

---
+++ code/Plugins/Nop.Plugin.Widgets.StoreLocation/StoreLocationPlugin.cs
@@ -0,0 +1,25 @@
+﻿using Nop.Services.Cms;
+using Nop.Services.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Plugin.Widgets.StoreLocation
+{
+    public class StoreLocationPlugin : BasePlugin, IWidgetPlugin
+    {
+        public bool HideInWidgetList => throw new NotImplementedException();
+
+        public string GetWidgetViewComponentName(string widgetZone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<string>> GetWidgetZonesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
